298ccbf53e5d82656efe66f299fed54744ea49c6
Zerve <Zerve0@hotmail.com>
2016-11-23 00:21:50 +0900

    typescript additions and refactoring

--- public/javascripts/soulsGame.js
+++ public/javascripts/soulsGame.js
@@ -1,61 +1,91 @@
-var renderer = PIXI.autoDetectRenderer(1280, 720, {backgroundColor : 0x10bb99});
-document.body.appendChild(renderer.view);
-
-var stage = new PIXI.Container();
-
-animate();
-
-function animate() {
-    requestAnimationFrame(animate);
-
-    renderer.render(stage);
-}
-
-//Class definitions
-//PlayerData class - holds all player related data
-function PlayerData(playerName) {
-	this.playerName = playerName;
-	this.currentLife = 0;
-	this.intensity = "";
-	this.power = 0;
-
-	this.personaCount = 0;
-	this.driveCount = 0;
-	this.hand = new CardZone(false);
-	this.terminus = new CardZone(true);
-	this.void = new CardZone(true);
-	this.inPlay = new CardZone(true);
-
-	//Loads all of the images for the deck
-	this.loadDeck() = function() {
-
-	}
-};
-
-//Card class - A does a lot of stuff
-function Card(entityID, cardID) {
-	this.entityID = entityID;
-	this.cardID = cardID;
-
-	//Start initialization logic
-
-	//End initilization logic
-	
-	this.addToStage() = function() {
-
-	}
-};
-
-//CardZone class - basically, a list of cards
-function CardZone(isVisible) {
-	this.isVisible = isVisible;
-	this.cardList = [];
-
-	this.addCard = function(card) {
-		this.cardList.push(card);
-	};
-
-	this.getLength = function() {
-		return cardList.length;
-	};
-};
+var Renderer;
+(function (Renderer) {
+    var renderer = PIXI.autoDetectRenderer(1280, 720, { backgroundColor: 0x10bb99 });
+    document.body.appendChild(renderer.view);
+    var stage = new PIXI.Container();
+    animate();
+    function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(stage);
+    }
+})(Renderer || (Renderer = {}));
+var Websocket;
+(function (Websocket) {
+    var websocket;
+    function initWebsocket(websocketPath) {
+        var websocketURL = "ws://" + window.location.host + websocketPath;
+        websocket = new WebSocket(websocketURL);
+        websocket.onopen = function (evt) { return onOpen(evt); };
+        websocket.onclose = function (evt) { return onClose(evt); };
+        websocket.onmessage = function (evt) { return onMessage(evt); };
+        websocket.onerror = function (evt) { return onError(evt); };
+    }
+    Websocket.initWebsocket = initWebsocket;
+    function onOpen(evt) {
+        writeToScreen("You have connected.");
+    }
+    function onClose(evt) {
+        writeToScreen("You have disconnected.");
+    }
+    function onMessage(evt) {
+        var json = jQuery.parseJSON(evt.data);
+        var type = json.type;
+        var html;
+        if (type == "message") {
+            html = json.user + ": " + json.message;
+        }
+        else {
+            html = "MSG ERROR: " + json.message;
+        }
+        writeToScreen(html);
+    }
+    function onError(evt) {
+        writeToScreen("ERROR: " + evt.data);
+    }
+    function writeToScreen(html) {
+        var htmlLine = html + "\n";
+        $('#gameLog').append(htmlLine);
+    }
+})(Websocket || (Websocket = {}));
+var GameObjects;
+(function (GameObjects) {
+    var PlayerData = (function () {
+        function PlayerData(playerName) {
+            this.playerName = playerName;
+            this.currentLife = 0;
+            this.intensity = "";
+            this.power = 0;
+            this.personaCount = 0;
+            this.driveCount = 0;
+            this.hand = new CardZone(false);
+            this.terminus = new CardZone(true);
+            this.void = new CardZone(true);
+            this.inPlay = new CardZone(true);
+        }
+        PlayerData.prototype.loadDeck = function () {
+        };
+        return PlayerData;
+    }());
+    var Card = (function () {
+        function Card(entityID, cardID) {
+            this.entityID = entityID;
+            this.cardID = cardID;
+        }
+        Card.prototype.addToStage = function () {
+        };
+        return Card;
+    }());
+    var CardZone = (function () {
+        function CardZone(isVisible) {
+            this.isVisible = isVisible;
+            this.cardList = [];
+        }
+        CardZone.prototype.addCard = function (card) {
+            this.cardList.push(card);
+        };
+        CardZone.prototype.getLength = function () {
+            return this.cardList.length;
+        };
+        return CardZone;
+    }());
+})(GameObjects || (GameObjects = {}));
---
+++ public/javascripts/soulsGame.ts
@@ -0,0 +1,132 @@
+/// <reference path="pixi.js.d.ts" />
+/// <reference path="jquery.d.ts" />
+
+//Renderer - all rendering related code
+module Renderer {
+	var renderer = PIXI.autoDetectRenderer(1280, 720, {backgroundColor : 0x10bb99});
+	document.body.appendChild(renderer.view);
+
+	var stage = new PIXI.Container();
+
+	animate();
+
+	function animate() {
+		requestAnimationFrame(animate);
+		renderer.render(stage);
+	}
+}
+
+//Websocket - the onmessage func will handle a lot of logic
+module Websocket {
+	var websocket;
+
+	export function initWebsocket(websocketPath) {
+		var websocketURL = "ws://" + window.location.host + websocketPath;
+		websocket = new WebSocket(websocketURL);
+
+		websocket.onopen = (evt) => onOpen(evt);
+		websocket.onclose = (evt) => onClose(evt);
+		websocket.onmessage = (evt) => onMessage(evt);
+		websocket.onerror = (evt) => onError(evt);
+	}
+
+	function onOpen(evt) {
+		writeToScreen("You have connected.");
+	}
+
+	function onClose(evt) {
+		writeToScreen("You have disconnected.");
+	}
+
+	function onMessage(evt) {
+		var json = jQuery.parseJSON(evt.data);
+		var type = json.type;
+		var html;
+
+		if(type == "message") {
+				html = json.user + ": " + json.message;
+		} else {
+				html = "MSG ERROR: " + json.message;
+		}
+
+		writeToScreen(html);
+	}
+
+	function onError(evt) {
+		writeToScreen("ERROR: " + evt.data);
+	}
+
+	function writeToScreen(html) {
+		var htmlLine = html + "\n";
+			$('#gameLog').append(htmlLine);
+	}
+}
+
+//Game Objects - helper objects for game related things
+module GameObjects {
+	//Class definitions
+	//PlayerData class - holds all player related data
+	class PlayerData {
+		playerName: string;
+		currentLife: number;
+		intensity: string;
+		power: number;
+		personaCount: number;
+		driveCount: number;
+		hand: CardZone;
+		terminus: CardZone;
+		void: CardZone;
+		inPlay: CardZone;
+
+		constructor(playerName: string) {
+			this.playerName = playerName;
+			this.currentLife = 0;
+			this.intensity = "";
+			this.power = 0;
+			this.personaCount = 0;
+			this.driveCount = 0;
+			this.hand = new CardZone(false);
+			this.terminus = new CardZone(true);
+			this.void = new CardZone(true);
+			this.inPlay = new CardZone(true);
+		}
+
+		//Loads all of the images for the deck
+		loadDeck() {
+		}
+	}
+
+	//Card class - A does a lot of stuff
+	class Card {
+		entityID: number;
+		cardID: number;
+
+		constructor(entityID: number, cardID: number) {
+			this.entityID = entityID;
+			this.cardID = cardID;
+		}
+		
+		
+		addToStage() {
+		}
+	}
+
+	//CardZone class - basically, a list of cards
+	class CardZone {
+		isVisible: boolean;
+		cardList: Card[];
+		
+		constructor(isVisible: boolean) {
+			this.isVisible = isVisible;
+			this.cardList = [];
+		}
+
+		addCard(card: Card) {
+			this.cardList.push(card);
+		}
+
+		getLength() {
+			return this.cardList.length;
+		}
+	}
+}
